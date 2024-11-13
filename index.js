c2109b0310727a4e6364a06f4964343533a42071
chinmay0910 <chinmayrmhatre@gmail.com>
2024-11-13 18:11:12 +0530

    added videos

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors'); 
 
 const app = express();
 const PORT = 8000; // You can change the port if needed
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Serve static files from the "videos" directory
 app.use('/videos', express.static(path.join(__dirname, 'videos')));
